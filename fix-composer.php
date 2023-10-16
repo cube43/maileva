9dec9cd95badd5befcef6b59a79290b967b6b871
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2023-10-16 17:09:45 +0200

    Initial commit

--- fix-composer.php
+++ fix-composer.php
@@ -2,10 +2,4 @@
 
 declare(strict_types=1);
 
-$composer = json_decode(file_get_contents('composer.json'), true);
-
-unset($composer['autoload-dev']);
-$composer['autoload']['psr-4'] = [];
-$composer['autoload']['psr-4']['Cube43\\Component\\MailEva\\'] = 'src/';
-
-file_put_contents('composer.json', json_encode($composer, JSON_PRETTY_PRINT));
+copy('composer_default.json', 'composer.json');
